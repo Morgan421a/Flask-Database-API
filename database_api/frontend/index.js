ee8bf5a93af2f4ac5a271680353d394d8dc0e711
Morgan421a <morgan.knight421a@gmail.com>
2026-02-16 09:22:27 +0000

    Input fields now stored in table; submit button reacts to input data

--- database_api/frontend/index.js
+++ database_api/frontend/index.js
@@ -1,9 +1,21 @@
 const inputBoxes = Array.from(document.getElementsByClassName("input"));
-const submit = document.getElementsByClassName("data-submit");
+const submitBtn = document.getElementsByClassName("data-submit")[0];
+
+inputBoxes.forEach(input => { // Checks each input value every time a user keystroke is detected
+    input.addEventListener("input", function() {
+        const values = inputBoxes.map(input => input.value);
+
+        const allFilled = values.every(value => value !== ""); // Checks if all fields have a value
+
+        submitBtn.disabled = !allFilled; // Disables submit button if allFilled = false
+        
+        submitBtn.value = allFilled ? "Submit" : "Please fill all fields first";
+    });
+});
 
 
 console.log(inputBoxes);
 
-const Table = fetch('http://127.0.0.1:5000/api/cars')
+const Table = fetch('http://127.0.0.1:5000/api/cars');
 
 
